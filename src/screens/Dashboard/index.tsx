519456bcd15e8959896c461b11d9bec0b542a2b7
Rodrigo Souza <rodrigoarsp@gmail.com>
2021-07-19 12:13:54 -0300

    Transaçoes dinamicas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { HighligthCard } from '../../components/HighlightCard';
 import { TransactionCard } from '../../components/TransactionCard';
 
@@ -15,10 +16,43 @@ import {
     HighligthCards,
     Transactions,
     Title,
-
+    TransactionList,
 } from './styles';
 
 export function Dashboard(){
+    const data =[
+    {
+        type: 'positive',
+        title: "Desenvolvimento de site",
+        amount: "R$ 12.000,00",
+        category: {
+            name: 'Vendas',
+            icon: 'dollar-sign',
+        },
+        date: "13/04/2020",
+    },
+    {
+        type: 'negative',
+        title: "Hamburgueria Pizzy",
+        amount: "R$ 12.000,00",
+        category: {
+            name: 'Alimentação',
+            icon: 'dollar-sign',
+        },
+        date: "10/04/2020",
+    },
+    {
+        type: 'negative',
+        title: "Aluguel do apartamento",
+        amount: "R$ 1.200,00",
+        category: {
+            name: 'Casa',
+            icon: 'dollar-sign',
+        },
+        date: "10/04/2020",
+    },
+
+]
     return(
         <Container >
             <Header>
@@ -57,7 +91,19 @@ export function Dashboard(){
 
             <Transactions>
                 <Title>Listagem</Title>
-                <TransactionCard />
+
+                <TransactionList 
+                    data={data}
+                    renderItem={({ item }) => <TransactionCard data={item}/>}
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        paddingBottom: getBottomSpace()
+                    }}
+                />
+
+                
+
+                
             </Transactions>
             
         </Container>
