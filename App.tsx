3be97f1a1ea72c426cbd34b413ec04a49d1dc1f1
Rodrigo Souza <rodrigoarsp@gmail.com>
2021-07-26 21:21:16 -0300

    Navegação

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
+
+import { NavigationContainer } from '@react-navigation/native';
 
 import { 
   useFonts, //função de recarregar as fonts (Hooks)
@@ -9,9 +13,8 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins';
 
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes';
+
 
 import theme from './src/global/styles/theme';
 
@@ -30,7 +33,9 @@ export default function App() {
 
   return(
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   ) 
 }
